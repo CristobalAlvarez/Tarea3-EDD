74c4ae33d3caef46b8e27e7dc7e6a3b8de34ba17
Cristobal Alvarez <cristobal@iMac-de-Cristobal.local>
2019-07-18 22:35:33 -0400

    Funciona! Solo debo comentar, corregir ciertos problemas y crear la funcion h y h2.

--- hashing.c
+++ hashing.c
@@ -90,16 +90,19 @@ int hashInsertOferta(oferta HT[], int k, int cant_desc, int desc) {
     int i;
     int pos = h(k,0);
     int inicio = pos;
-    for (i = 1; HT[pos].cod_producto != VACIA && HT[pos].cod_producto != k; i++)
+
+    for (i = 1; HT[pos].cod_producto != VACIA && HT[pos].cod_producto != k; i++){
         pos = (inicio + h(k, i)) % CANT_OFERTA;
-        if (HT[pos].cod_producto == k){
-            return 0;
-        }else{
-            HT[pos].cod_producto = k;
-            HT[pos].cantidad_descuento = cant_desc;
-            HT[pos].descuento = desc;
-            return 1;
-        }
+    }
+
+    if (HT[pos].cod_producto == k){
+        return 0;
+    }else{
+        HT[pos].cod_producto = k;
+        HT[pos].cantidad_descuento = cant_desc;
+        HT[pos].descuento = desc;
+        return 1;
+    }
 }
 
 int hashInsertProducto(producto HT[], int k, char nombre[31], int pre) {
@@ -108,26 +111,33 @@ int hashInsertProducto(producto HT[], int k, char nombre[31], int pre) {
     int inicio = pos;
     for (i = 1; HT[pos].cod_producto != VACIA && HT[pos].cod_producto != k; i++)
         pos = (inicio + h(k, i)) % CANT_PRODUCTO;
-        if (HT[pos].cod_producto == k){
-            return 0;
-        }else{
-            HT[pos].cod_producto = k;
-            HT[pos].precio = pre;
-            strcpy(HT[pos].nbre_producto,nombre);
-            return 1;
-        }
+
+    if (HT[pos].cod_producto == k){
+        return 0;
+    }else{
+        HT[pos].cod_producto = k;
+        HT[pos].precio = pre;
+        strcpy(HT[pos].nbre_producto,nombre);
+        return 1;
+    }
 }
 
 producto searchProducto(producto HP[],int k){
     int inicio, i;
     int pos = h(k,0);
     inicio=pos;
+
     producto output;
+    output.cod_producto = 0;
+    output.precio = 0;
+    strcpy(output.nbre_producto,"nada");
+
     for (i = 1; HP[pos].cod_producto != VACIA && HP[pos].cod_producto != k; i++){
-        pos = (inicio + h(k, i)) % CANT_PRODUCTO; // próxima ranura en la secuencia
-        if (HP[pos].cod_producto == k){
-            output = HP[pos]; // registro encontrado, búsqueda exitosa
-        }
+        pos = (inicio + h(k, i)) % CANT_PRODUCTO;
+    }
+    
+    if (HP[pos].cod_producto == k){
+        output = HP[pos];
     }
 
     return output;
@@ -137,22 +147,30 @@ oferta searchOferta(oferta HP[],int k){
     int inicio, i;
     int pos = h(k,0);
     inicio=pos;
+
     oferta output;
-    for (i = 1; HP[pos].cod_producto != VACIA && HP[pos].cod_producto != k; i++){
+    output.cod_producto = 0;
+    output.cantidad_descuento = 0;
+    output.descuento = 0;
+
+    for(i = 1; HP[pos].cod_producto != VACIA && HP[pos].cod_producto != k; i++){
         pos = (inicio + h(k, i)) % CANT_OFERTA; // próxima ranura en la secuencia
-        if (HP[pos].cod_producto == k){
-            output = HP[pos]; // registro encontrado, búsqueda exitosa
-        }
+    }
+
+    if(HP[pos].cod_producto == k){
+            output = HP[pos];
+            return output;
     }
 
     return output;
 }
 
+
 void hashDisplayOferta(oferta HT[]){
    int i = 0;
    for(i = 0; i<CANT_OFERTA; i++) {
       if(HT[i].cod_producto != VACIA)
-         printf(" (%d,%d)",HT[i].cod_producto,HT[i].descuento);
+         printf(" (Codigo: %d, Descuento: %d. Cantidad: %d)",HT[i].cod_producto,HT[i].descuento,HT[i].cantidad_descuento);
       else
          printf(" ~~ ");
    }
@@ -170,8 +188,25 @@ void hashDisplayProducto(producto HT[]){
    printf("\n");
 }
 
+int doDescuento(oferta HT[], int k){
+    int i;
+    int pos = h(k,0);
+    int re=0;
+    int inicio=pos;
+    for(i = 1; HT[pos].cod_producto != VACIA && HT[pos].cod_producto != k; i++){
+        pos = (inicio + h(k, i)) % CANT_OFERTA;
+    }
+        
+    if(HT[pos].cod_producto == k && HT[pos].cantidad_descuento > 0){
+        (&HT)[pos]->cantidad_descuento--;
+        re = HT[pos].descuento;
+    }
+    return re;
+
+}
+
 int main(){
-    int ofertCant,i,j;
+    int i,j;
     char buffer[100];
 
     oferta *inputOfertas = hashInitOferta();
@@ -179,18 +214,28 @@ int main(){
 
     FILE *input;
     input = fopen("compras.txt","r");
+
     fgets(buffer,100,input);
     strtok(buffer, "\n");
     int cantCompras = atoi(buffer);
+    printf("%s\n",buffer);
 
     for(i=0;i<cantCompras;i++){
+        int total = 0;
         fgets(buffer,100,input);
         strtok(buffer, "\n");
         int compras = atoi(buffer);
         for(j=0;j<compras;j++){
             fgets(buffer,100,input);
-            printf("%s",buffer);
+            int id = atoi(buffer);
+            //printf("Producto comprado:%s. Codigo:%s\n",searchProducto(inputProductos,atoi(buffer)).nbre_producto,buffer);
+            total = total + searchProducto(inputProductos,id).precio - doDescuento(inputOfertas,id);
         }
+        printf("%d\n",total);
     }
-    
+
+    free(inputOfertas);
+    free(inputProductos);
+    fclose(input);
+
 }
